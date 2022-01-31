77890d32dca1c33f618b9cf62cecc259a2f9b562
Sayed <sayed96@hotmail.de>
2022-01-31 01:08:47 +0100

    finished code

--- js/app.js
+++ js/app.js
@@ -24,54 +24,58 @@
 */
 let navbar = document.getElementById("navbar__list");
 let mainSectionCount = document.getElementsByTagName("section");
+let test = document.getElementById("section2");
 /* let mainChildren = mainSectionCount.getElementsByTagName("section"); */
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+/* 
+This function creates buttons depending on how many sections the page contains and adds them to the navbar
+It also takes the custom data-* attribute of each section and adds it to the buttons innerHTML
+*/
+function addButton(){
 
-console.log(navbar);
-for(const e of mainSectionCount){
- let navbarButton = document.createElement("button");
- navbarButton.innerHTML = e.dataset.nav;
- navbar.appendChild(navbarButton);
+ for(const e of mainSectionCount){
+  let navbarButton = document.createElement("button");
+  navbarButton.innerHTML = e.dataset.nav;
+  navbar.appendChild(navbarButton);
+ }
+
 }
+addButton();
+/* 
+When clicking a button in the navbar, this eventlistener generates the ID of the section, you want to scroll to, by slicing the buttons innerHTML.
 
+Can you tell me in your review if this performs better than anchoring the buttons to the sections?
+*/
 navbar.addEventListener("click", event =>{
- target = event.target;
+ const target = event.target;
+
  if(target.matches("button")){
   let sectionNumber = target.innerHTML.slice(-1);
   document.getElementById("section" + sectionNumber).scrollIntoView();
  }
+
+})
+
+/* 
+This eventlistener checks, when you scroll
+*/
+document.addEventListener("scroll", event =>{ 
+ 
+ for(element of mainSectionCount){
+   let rect = element.getBoundingClientRect().top;
+   if(rect < 500 && rect > -250){
+    element.classList.add("your-active-class");
+   }else{
+    element.classList.remove("your-active-class");
+   }
+ }
+
 })
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
